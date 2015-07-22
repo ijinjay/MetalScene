049734e1a29d4269aef6bafc3d99cad7139d4e8d
Jin Jay <709134513@qq.com>
2015-07-22 15:45:25 +0800

    Initial Commit

---
+++ MetalScene/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MetalScene
+//
+//  Created by JinJay on 15/7/22.
+//  Copyright © 2015年 JinJay. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MetalScene/GameViewController.h
@@ -0,0 +1,17 @@
+//
+//  GameViewController.h
+//  MetalScene
+//
+//  Created by JinJay on 15/7/22.
+//  Copyright © 2015年 JinJay. All rights reserved.
+//
+
+@import UIKit;
+@import Metal;
+@import MetalKit;
+
+@interface GameViewController : UIViewController<MTKViewDelegate>
+
+@end
+
+
